1839a620684d208b576bcd08fc3808329ba469d3
renatogual <renatogual@hotmail.com>
2021-08-30 16:01:43 -0300

    Criado header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import SignInButton from "../SignInButton";
+import logo from "../../../public/images/logo.svg";
+
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logo} alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
