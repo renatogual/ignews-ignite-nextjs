420b323e7f4f54cc4339d9eadddd55c1bf229d82
renatogual <renatogual@hotmail.com>
2021-08-30 16:25:24 -0300

    Criado pagina home e botao subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import SubscribeButton from "../components/SubscribeButton";
+import avatar from "../../public/images/avatar.svg";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +12,20 @@ export default function Home() {
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1>Hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <Image src={avatar} alt="Girl coding" />
+      </main>
     </>
   );
 }
