229234cf9845a34d8522380fe8afd19590c12fa1
renatogual <renatogual@hotmail.com>
2021-09-07 16:17:22 -0300

    Correções erros de digitação

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,11 +21,11 @@ export default NextAuth({
           q.If(
             q.Not(
               q.Exists(
-                q.Match(q.Index("users_by_email"), q.Casefold(user.email))
+                q.Match(q.Index("user_by_email"), q.Casefold(email))
               )
             ),
             q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
           )
         );
 
